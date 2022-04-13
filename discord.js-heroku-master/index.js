74c66454071d21828ca2bb62128e503900d02637
Polarp1 <102926701+Polarp1@users.noreply.github.com>
2022-04-12 19:58:51 -0500

    Update index.js

--- discord.js-heroku-master/index.js
+++ discord.js-heroku-master/index.js
@@ -6,14 +6,8 @@ client.on('ready', () => {
     client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
 });
 
-bot.on("message", function (msg) {
-	// if message begins with "ping"
-	if (msg.content.indexOf("ping") === 0) {
-		// send a message to the channel the ping message was sent in.
-		bot.sendMessage(msg.channel, "pong!");
-
-		// alert the console
-		console.log("pong-ed " + msg.author.username);
+bot.on("message") {
+	message.channel.send (`${client.ws.ping} ms`)
 	}
 });
 

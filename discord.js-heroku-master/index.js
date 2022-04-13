4284423a930d7ef857b8b8a4cdd319542d7c746c
Polarp1 <102926701+Polarp1@users.noreply.github.com>
2022-04-13 17:53:22 -0500

    Update index.js

--- discord.js-heroku-master/index.js
+++ discord.js-heroku-master/index.js
@@ -14,4 +14,11 @@ client.on("message", msg => {
 })
 
 
+client.on("message") {
+        msg.reply("Bzzt bzzt, *a robotic voice begins reading you letters.* https://twitter.com/Unknown_Us3r5?s=20&t=qaM1V1KEHqnyn9gHySNH2A")
+    }  
+         
+})
+
+
 client.login(process.env.TOKEN);

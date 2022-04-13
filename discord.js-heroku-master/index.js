5f8104e28e0d17dc939d4aff83654b9a09c11c44
Polarp1 <102926701+Polarp1@users.noreply.github.com>
2022-04-12 19:59:49 -0500

    Update index.js

--- discord.js-heroku-master/index.js
+++ discord.js-heroku-master/index.js
@@ -6,9 +6,6 @@ client.on('ready', () => {
     client.user.setActivity('https://git.io/d.js-heroku', {type: 'WATCHING'});
 });
 
-bot.on("message") {
-	message.channel.send (`${client.ws.ping} ms`)
-	}
-});
+
 
 client.login(process.env.TOKEN);

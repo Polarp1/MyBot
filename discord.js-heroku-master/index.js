37e4fdb0b28bd917fb1c66a4b7dbfdeebfa733b3
Polarp1 <102926701+Polarp1@users.noreply.github.com>
2022-04-12 20:24:19 -0500

    Update index.js

--- discord.js-heroku-master/index.js
+++ discord.js-heroku-master/index.js
@@ -7,8 +7,8 @@ client.on('ready', () => {
 });
 
 client.on("message", msg => {
-    if (msg.content === "ping") {
-        msg.reply("pong")
+    if (msg.content === "congrats") {
+        msg.reply("Bzzt bzzt, *a robotic voice begins reading you letters.* https://twitter.com/Unknown_Us3r5?s=20&t=qaM1V1KEHqnyn9gHySNH2A")
     }  
          
 })
